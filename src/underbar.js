be8596dfca2fc4b7cfeedef50759b6b16b863bc7
Robert Holmes <rwholmes1@gmail.com>
2014-01-14 17:33:11 -0800

    Completed the _.map and _.pluck functions

--- src/underbar.js
+++ src/underbar.js
@@ -123,6 +123,11 @@ var _ = { };
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var outputarr = [];
+    for (var i=0; i<array.length; i++) {
+      outputarr.push(iterator(array[i]));
+    }
+    return outputarr;
   };
 
   /*
