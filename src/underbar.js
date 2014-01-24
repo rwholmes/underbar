799a8f4146e6cdc58482d60b78da40f0ef09e657
Robert Holmes <rwholmes1@gmail.com>
2014-01-24 11:35:34 -0800

    Completed the solution for the _.defaults function

--- src/underbar.js
+++ src/underbar.js
@@ -304,6 +304,15 @@ var _ = { };
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var sources = Array.prototype.slice.call(arguments,1);
+    _.each(sources, function(item) {
+      _.each(item, function(value, key) {
+        if (obj[key] === undefined) {
+          obj[key] = value;
+        }
+      });
+    });
+    return obj;
   };
 
 
